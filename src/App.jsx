aa44870821f467747e787e97af09f0b9d8b85b34
dce5637-cmd <dce5637@gmail.com>
2026-05-11 16:19:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,7 @@ export default function App() {
         HOLIDAYDOPE WEBSITE
       </div>
 
-      <a
-        href="https://wa.me/918595789670"
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{
-          position: "fixed",
-          bottom: "22px",
-          right: "22px",
-          background: "#25D366",
-          color: "white",
-          padding: "14px 18px",
-          borderRadius: "50px",
-          fontWeight: "bold",
-          textDecoration: "none",
-          boxShadow: "0 8px 25px rgba(0,0,0,0.3)",
-          zIndex: 9999
-        }}
-      >
-        WhatsApp Now
-      </a>
+      
     </>
   );
 }
